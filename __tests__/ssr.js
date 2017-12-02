9b1f33b0ea13de184c01b338682e7d698c027140
Paul Salaets <psalaets@gmail.com>
2017-12-01 19:59:22 -0500

    some tests for production mode in ssr

--- __tests__/ssr.js
+++ __tests__/ssr.js
@@ -56,6 +56,71 @@ describe('server side rendering', () => {
       });
     });
   });
+
+  describe('settings.production === true', () => {
+    it('does not add attribute to element', () => {
+      const instance = new Vue(component('address'));
+      const renderer = createRenderer({
+        directives: {
+          test: plugin.ssr({
+            production: true
+          })
+        }
+      });
+
+      return renderer.renderToString(instance, (err, html) => {
+        if (err) throw err;
+
+        expect(html).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('settings.production === false', () => {
+    it('adds attribute to element', () => {
+      const instance = new Vue(component('address'));
+      const renderer = createRenderer({
+        directives: {
+          test: plugin.ssr({
+            production: false
+          })
+        }
+      });
+
+      return renderer.renderToString(instance, (err, html) => {
+        if (err) throw err;
+
+        expect(html).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('settings.production not set', () => {
+    let oldNodeEnv = process.env.NODE_ENV;
+
+    beforeEach(() => {
+      process.env.NODE_ENV = 'production';
+    });
+
+    afterEach(() => {
+      process.env.NODE_ENV = oldNodeEnv;
+    });
+
+    it('uses process.env.NODE_ENV to determine production', () => {
+      const instance = new Vue(component('address'));
+      const renderer = createRenderer({
+        directives: {
+          test: plugin.ssr({})
+        }
+      });
+
+      return renderer.renderToString(instance, (err, html) => {
+        if (err) throw err;
+
+        expect(html).toMatchSnapshot();
+      });
+    });
+  });
 });
 
 function component(name) {
